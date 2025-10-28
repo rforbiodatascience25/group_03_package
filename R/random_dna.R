77270a2b92af790f9c27996bc37884f6358fabd9
Marco-LH <mbonafedelh@gmail.com>
2025-10-28 10:39:34 +0100

    Correction of function 1

--- R/random_dna.R
+++ R/random_dna.R
@@ -5,9 +5,6 @@
 #' @returns Returns a random DNA-like sequence with the input lenght
 #' @export
 #'
-#' @examples 10 is the input by the user, the function will create a one continue
-#'string of random DNA-like with a lenght of 10 nucleotide, example of an output: ACCTGAAGCT
-#'
 random_dna1 <- function(random_dna2){
   random_dna3 <- sample(c("A", "T", "G", "C"), size = random_dna2, replace = TRUE)
   random_dna4 <- paste0(random_dna3, collapse = "")
